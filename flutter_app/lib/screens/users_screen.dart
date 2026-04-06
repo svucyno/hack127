967160b7e83d0638853c0661fc2b8ddde153192f
sameerreddy789 <sameer1995jagada@gmail.com>
2026-04-06 12:08:33 +0530

    Add landing page (about.html): hero, 12 feature cards, 4 role cards, impact stats, how-it-works flow, tech stack, CTA

--- flutter_app/lib/screens/users_screen.dart
+++ flutter_app/lib/screens/users_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import '../widgets/app_drawer.dart';
 
 class UsersScreen extends StatelessWidget {
